acd1c51e74d68f295cb4397509417c05c6c57be7
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-17 18:50:47 +0100

    try catch modified

--- Main.java
+++ Main.java
@@ -4,27 +4,34 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Podaj PESEL");
-        String pesel = sc.next();
 
-        if(String.valueOf(pesel).length() != 11) {
-            throw new Exception("PESEL musi zawierac 11 liczb");
+        try {
+         String pesel = sc.next();
+         Length(pesel);
+         containsDigits(pesel);
+        } catch (IllegalArgumentException ex)
+        {
+            throw new IllegalArgumentException(ex);
         }
-        if(!containsDigits(pesel)){
-            throw new Exception("Podano litere zamiast cyfry");
-        }
-
     }
 
-    public static boolean containsDigits(String pesel) {
+    public static void containsDigits(String pesel) {
 
         char[] peselAr = pesel.toCharArray();
         for(int i=0; i<peselAr.length;i++)
-            if((int)peselAr[i] < 48 || (int)peselAr[i] > 57) return false;
-        return true;
+            if((int)peselAr[i] < 48 || (int)peselAr[i] > 57)
+                throw new IllegalArgumentException("Podano litere zamiast cyfry");
+
+    }
+
+    public static void Length(String pesel) {
+        if (String.valueOf(pesel).length() != 11) {
+            throw new IllegalArgumentException("PESEL musi zawierac 11 liczb");
+        }
     }
 }
